3dcdd76ee26a2ce52deb6f85d56fdcc409a6e908
Thomas Klemm <mail@tklemm.eu>
2012-01-25 22:34:18 +0100

    activity light instead of time_ago_in_words in view

--- app/helpers/repo_helper.rb
+++ app/helpers/repo_helper.rb
@@ -1,2 +1,22 @@
 module RepoHelper
+  
+  # time_ago_in_words(time) as view helper possible
+  
+  def activity_light(last_updated)
+    time_difference = Time.now - last_updated
+    
+    if time_difference < (18*24*60*60)
+      return "activity awesomely_active"
+    elsif time_difference < (38*24*60*60)
+      return "activity greatly_active"
+    elsif time_difference < (92*24*60*60)
+      return "activity active"
+    elsif time_difference < (185*24*60*60)
+      return "activity less_active"
+    elsif time_difference > (185*24*60*60)
+      return "activity not_active"
+    end
+    
+  end
+  
 end
