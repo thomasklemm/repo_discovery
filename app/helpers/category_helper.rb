67c9226dafe84ffb2e77d8f723682d23da98cc08
Thomas Klemm <mail@tklemm.eu>
2012-01-28 16:22:34 +0100

    changes in category view

--- app/helpers/category_helper.rb
+++ app/helpers/category_helper.rb
@@ -18,14 +18,4 @@ module CategoryHelper
     return top_repos.join(", ")
   end
   
-  def print_total_watchers_for_category(cat_name)
-    repos = Repo.tagged_with(cat_name, on: :categories).order("watchers DESC")
-    watchers = 0
-    repos.each do |repo|
-      watchers += repo.watchers
-    end
-    
-    return watchers
-  end
-  
 end
