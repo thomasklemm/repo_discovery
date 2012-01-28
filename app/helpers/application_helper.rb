151c85202a920990ec01b620c99e6b789cf516a0
Thomas Klemm <mail@tklemm.eu>
2012-01-28 15:34:32 +0100

    tag/category page designed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  
+  def day_name_today
+    t = Time.now
+    return t.strftime("%A")
+  end
+  
+  def is_active?(cont_name)
+    c = controller.controller_name
+    if c == cont_name
+      return "active"
+    else
+    end
+  end
+  
 end
--- app/helpers/category_helper.rb
+++ app/helpers/category_helper.rb
@@ -8,4 +8,24 @@ module CategoryHelper
     end
   end
   
+  def print_top_repos_for_category(cat_name)
+    repos = Repo.tagged_with(cat_name, on: :categories).order("watchers DESC")
+    top_repos = Array.new
+    repos.each do |repo|
+      top_repos << repo.name
+    end
+    
+    return top_repos.join(", ")
+  end
+  
+  def print_total_watchers_for_category(cat_name)
+    repos = Repo.tagged_with(cat_name, on: :categories).order("watchers DESC")
+    watchers = 0
+    repos.each do |repo|
+      watchers += repo.watchers
+    end
+    
+    return watchers
+  end
+  
 end
